a6f20ee963cbadc9621944d985cca0e4508d802c
GONGRYUNG <alsgud413@naver.com>
2022-12-06 19:42:53 +0900

    민형 프사업뎃

--- 프론트엔드/front-end/src/9.Chat/ChatForm.js
+++ 프론트엔드/front-end/src/9.Chat/ChatForm.js
@@ -3,6 +3,17 @@ import React from "react";
 const ChatForm = ({ handleSubmit, text, setText, setImg }) => {
     return (
         <form onSubmit={handleSubmit}>
+                  <label htmlFor="img">
+        {/* <Attachment /> */}
+      </label>
+      <input
+        onChange={(e) => setImg(e.target.files[0])}
+        type="file"
+        id="img"
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+
         <div>
             <input
                 type="text"
--- 프론트엔드/front-end/src/9.Chat/User.js
+++ 프론트엔드/front-end/src/9.Chat/User.js
@@ -1,11 +1,57 @@
-import React, { useEffect, useState } from "react";
-// import Img from "../image1.jpg";
+import React, { useEffect, useId, useState } from "react";
+// import Img from "../../";
 import { onSnapshot, doc } from "firebase/firestore";
 import { db } from "../firebase";
+import { storage } from "../firebase";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import Cookies from 'universal-cookie';
+import TeamAPI from "../0. API/TeamAPI";
 
 const User = ({ user1, user, selectUser, chat }) => {
+  const cookies = new Cookies();
   const user2 = user?.uid;
   const [data, setData] = useState("");
+  const [image, setImage] = useState(null);
+  const localId = cookies.get('rememberId');
+  const [url, setUrl] = useState(null);
+  
+
+  const onSaveFace = async() => {
+    const imageRef = ref(storage, localId);
+
+    uploadBytes(imageRef, image).then(() => {
+      getDownloadURL(imageRef).then(async(url) => {
+        console.log("\nURL : " + url);
+        setUrl(url);
+
+      /* ----- (시작) 통신 ----- */
+        try {
+          const response = await TeamAPI.changeFace(url, localId);
+          console.log(response.data.result);
+          if(response.status == 200) {
+            console.log("통신 성공(200)");
+            alert("프사 저장 성공");
+          } else {
+            console.log("\n>> 통신 실패 : " + response.status);
+            alert("통신 실패 : " + response.status);
+          }
+        } catch (e) {
+          console.log(e);
+          console.log("캐치 !! 이미지 url 저장 실패..");
+        } // try-catch 문의 끝
+      /* ----- (끝) 통신 ----- */
+
+      }).catch((error) => {
+        console.log(error.message, "error getting the image url");
+      });
+      setImage(null);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+
+    console.log("순서가 이상한 URL : " + url);
+  };
+
 
   useEffect(() => {
     const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
@@ -14,6 +60,8 @@ const User = ({ user1, user, selectUser, chat }) => {
     });
     return () => unsub();
   }, []);
+
+
 
   // console.log(data);
 
@@ -47,11 +95,12 @@ const User = ({ user1, user, selectUser, chat }) => {
         onClick={() => selectUser(user)}
         className={`sm_container ${chat.nickName === user.nickName && "selected_user"}`}
       >
-        {/* <img
-          src={user.avatar}
+        <img
+        
+          src={`https://firebasestorage.googleapis.com/v0/b/isour-c9756.appspot.com/o/${user.id}?alt=media&token=` }
           alt="avatar"
           className="avatar sm_screen"
-        /> */}
+        />
       </div>
     </>
   );
